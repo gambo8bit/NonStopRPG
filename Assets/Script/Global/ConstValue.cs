87bbade32e87ed7d969d8caa300df9595eeea344
GAMBO8BIT <gambo8bit@gmail.com>
2017-11-13 17:49:06 +0900

    no message

--- Assets/Script/Global/ConstValue.cs
+++ Assets/Script/Global/ConstValue.cs
@@ -13,5 +13,10 @@ public class ConstValue
 
     //GetData Key
     public const string ActorData_Team = "TEAM_TYPE";
-	
+
+
+
+
+    //Path Key
+    public const string CharacterTemplatePath = "JSON/CHARACTER_TEMPLATE";  //캐릭터 스테이터스 템플릿 경로
 }
--- Assets/Script/Manager/CharacterStatusManager.cs
+++ Assets/Script/Manager/CharacterStatusManager.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SimpleJSON;
 
 public class CharacterStatusManager : MonoSingleton<CharacterStatusManager>
 {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Dictionary<string, CharacterTemplateData> DicTemplate = new Dictionary<string, CharacterTemplateData>();
+
+    private void Awake()
+    {
+        TextAsset characterText = Resources.Load(ConstValue.CharacterTemplatePath) as TextAsset;    //캐릭터스테이터스 제이슨파일 텍스트로 가져옴
+
+        if(characterText != null)   //제이슨파일 로드 제대로 했다면
+        {
+            JSONObject rootNodeText = JSON.Parse(characterText.text) as JSONObject; //파싱 <조사해볼것>
+
+
+            if(rootNodeText != null)    //파싱 제대로 됐다면
+            {
+                JSONObject 
+            }
+
+
+        }
+    }
 }
